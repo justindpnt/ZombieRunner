6901052b2b6abb351eb90253554e172b36a8f601
Justin <justindpnt@gmail.com>
2020-02-18 21:20:41 -0800

    Modified Sandbox with more hostiles, and can now aim down sight

---
+++ Zombie Runner/Assets/Scripts/AimScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This script controls the gun model transform change. Ideally, this and WeaponZoom would be one class,
+// however that proved to be a struggle for many hours until I learned the difference between 
+// transform.position, and transform.localposition. For now, it stays
+
+public class AimScript : MonoBehaviour
+{
+    [SerializeField] Canvas crosshair;
+
+    public Vector3 aimDownSights;
+    public Vector3 hipFire;
+    float aimSpeed = 100f;
+
+    bool aimDownSightsToggle = false;
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (aimDownSightsToggle == false)
+            {
+                aimDownSightsToggle = true;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, aimDownSights, aimSpeed * Time.deltaTime);
+                crosshair.enabled = false;
+            }
+            else
+            {
+                aimDownSightsToggle = false;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, hipFire, aimSpeed * Time.deltaTime);
+                crosshair.enabled = true;
+            }
+        }
+        
+    }
+}
--- Zombie Runner/Assets/Scripts/WeaponZoom.cs
+++ Zombie Runner/Assets/Scripts/WeaponZoom.cs
@@ -2,31 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// This script controls the camera zoom on the scope in. Ideally, this and AimScript would be one class,
+// however that proved to be a struggle for many hours until I learned the difference between 
+// transform.position, and transform.localposition. For now, it stays
+
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
-    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedInFOV = 45f;
 
-    bool zoomedInToggle = false;
+    bool aimDownSightsToggle = false;
 
     private void Update()
     {
+        
         if (Input.GetMouseButtonDown(1))
         {
-            if (zoomedInToggle == false)
+            if (aimDownSightsToggle == false)
             {
-                Debug.Log("I gete here");
-                zoomedInToggle = true;
+                aimDownSightsToggle = true;
                 fpsCamera.fieldOfView = zoomedInFOV;
             }
             else
             {
-                zoomedInToggle = false;
+                aimDownSightsToggle = false;
                 fpsCamera.fieldOfView = zoomedOutFOV;
             }
-        }
+        } 
     }
-
-
 }
