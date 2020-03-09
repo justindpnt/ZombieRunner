fb2439a6ab08a35345d76cc42b8691bf068f0fdf
Justin <justindpnt@gmail.com>
2020-03-08 22:12:57 -0700

    Cleaned up weapon zoom. Removed non essential weapon asssets

--- Zombie Runner/Assets/Scripts/WeaponZoom.cs
+++ Zombie Runner/Assets/Scripts/WeaponZoom.cs
@@ -11,19 +11,20 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
+    [SerializeField] RigidbodyFirstPersonController fpsController;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 45f;
     [SerializeField] float zoomOutSensitivity = 2f;
     [SerializeField] float zoomInSensitivity = .5f;
 
-    RigidbodyFirstPersonController fpsController;
+    [SerializeField] Canvas crosshair;
+
+    //Vector3 aimDownSights = new Vector3(-.06f, -.045f, .4f); //The vector that makes it show correctly in unity fullscreen
+    public Vector3 aimDownSights = new Vector3(0f, -.07f, .4f); //The vector that makes it show correctly in a web build
+    public Vector3 hipFire = new Vector3(.158f, -.162f, .533f);
+    float aimSpeed = 100f;
 
     bool aimDownSightsToggle = false;
-
-    private void Start()
-    {
-        fpsController = GetComponent<RigidbodyFirstPersonController>();
-    }
 
     private void Update()
     {
@@ -36,6 +37,9 @@ public class WeaponZoom : MonoBehaviour
                 fpsCamera.fieldOfView = zoomedInFOV;
                 fpsController.mouseLook.XSensitivity = zoomInSensitivity;
                 fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+                aimDownSightsToggle = true;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, aimDownSights, aimSpeed * Time.deltaTime);
+                crosshair.enabled = false;
             }
             else
             {
@@ -43,6 +47,9 @@ public class WeaponZoom : MonoBehaviour
                 fpsCamera.fieldOfView = zoomedOutFOV;
                 fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
                 fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+                aimDownSightsToggle = false;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, hipFire, aimSpeed * Time.deltaTime);
+                crosshair.enabled = true;
             }
         } 
     }
